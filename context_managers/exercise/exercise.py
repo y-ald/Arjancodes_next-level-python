a894263bfe20170ac9cf6d560df44beb0a399aca
Andreas Bergman <andreasbergman@MacBook-Air.local>
2023-11-17 15:02:14 +0100

    Update descriptions for exercises

--- context_managers/exercise/exercise.py
+++ context_managers/exercise/exercise.py
@@ -1,10 +1,17 @@
-# Implement a context manager decorator named file_manager
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Exercise: Implement a context manager decorator named file_manager
+# Implement a context manager decorator named file_manager that allows you to open a file
+# and write content to it. Use this decorator to write the content "Hello, world!" to the file.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 
 def write_file_content(content: str):
     with file_manager("sample.txt", "w") as file:
         file.write(content)
-        
+
+
 def main() -> None:
     write_file_content("Hello, world!")
 
--- iterators/exercise/exercise1.py
+++ iterators/exercise/exercise1.py
@@ -1,22 +1,29 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Exercise: Use itertools to generate combinations and calculate averages
+# Use itertools to generate all possible combinations of length 2 from the data list.
+# Calculate the average of each combination and store the results in a new list.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 from typing import Collection
 
 
-# This function calculates the average of a list of numbers
 # Note: I'm using the Collection type hint here, which is a generic type hint
 # for collections/iterables that support the len() function.
 def calculate_average(numbers: Collection[int]) -> float:
     total = sum(numbers)
     return total / len(numbers)
 
-def calculate_combination_averages(data: list):
-    # TODO: Use itertools to generate all possible combinations of length 2 from the data list
 
+def calculate_combination_averages(data: list) -> list[float]:
+    # TODO: Use itertools to generate all possible combinations of length 2 from the data list
     # TODO: Calculate the average of each combination and store the results in a new list
     ...
 
+
 def main() -> None:
     data = [1, 2, 3, 4, 5]
-
 
     averages: list[float] = calculate_combination_averages(data)
 
